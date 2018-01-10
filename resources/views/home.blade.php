d39a07836787ff96dbbdce6f1dfb3ece5e5752e5
Nikita Chalenko <nikita.chalenko@cloudbeds.com>
2018-01-10 22:11:56 +0200

    not doubles on the main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,10 +30,12 @@
 
     <div class="container marketing">
         <div class="row">
+            @php($doubles = [])
             @php($i = 0)
             @foreach($posts as $post)
                 @if($post->circle && $i < 3)
                     @php($i++)
+                    @php($doubles[] = $post->id)
                     <div class="col-lg-4">
                         <img class="img-circle" src="{{ URL::asset('/images/' . $post->id . '__circle_image.jpg') }}" alt="Generic placeholder image" width="140" height="140">
                         <h1>{!! $post->title !!}</h1>
@@ -45,18 +47,20 @@
         </div>
         @php($i = 0)
         @foreach($posts as $post)
-            @php($i++)
-            <hr class="featurette-divider">
-            <div class="row featurette">
-                <div class="col-md-7 @if ($i % 2) col-md-push-5 @endif">
-                    <h2 class="featurette-heading">{{ $post->title }}</h2>
-                    <h3><span class="text-muted">{!! $post->header !!}</span></h3>
-                    <p><a class="btn btn-lg btn-primary" href="/posts/{{ $post->id }}" role="button">Читать</a></p>
+            @if(!in_array($post->id, $doubles))
+                @php($i++)
+                <hr class="featurette-divider">
+                <div class="row featurette">
+                    <div class="col-md-7 @if ($i % 2) col-md-push-5 @endif">
+                        <h2 class="featurette-heading">{{ $post->title }}</h2>
+                        <h3><span class="text-muted">{!! $post->header !!}</span></h3>
+                        <p><a class="btn btn-lg btn-primary" href="/posts/{{ $post->id }}" role="button">Читать</a></p>
+                    </div>
+                    <div class="col-md-5 @if ($i % 2) col-md-pull-7 @endif">
+                        <img class="featurette-image img-responsive center-block" src="{{ URL::asset('/images/' . $post->id . '__500_image.jpg') }}" alt="Generic placeholder image">
+                    </div>
                 </div>
-                <div class="col-md-5 @if ($i % 2) col-md-pull-7 @endif">
-                    <img class="featurette-image img-responsive center-block" src="{{ URL::asset('/images/' . $post->id . '__500_image.jpg') }}" alt="Generic placeholder image">
-                </div>
-            </div>
+            @endif
         @endforeach
 
 {{--</div><!-- /.container -->--}}
